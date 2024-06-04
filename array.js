885f284668ab8249f3fbba6ff0c98a51641557cf
Mahnoor-Anwer <mahnooranwer13@gmail.com>
2024-06-03 20:01:39 -0700

    first commit

---
+++ array.js
@@ -0,0 +1,18 @@
+var fruits = ['mangoes', 'orange', 'kiwi', 'grapes', 'apple'];
+console.log(fruits[1]);
+//fruits.push('apple')
+//console.log(fruits)
+//fruits.pop()
+//console.log(fruits)
+//let f2 = fruits.pop()
+//console.log(fruits)
+//console.log(fruits.length)
+//fruits.shift()
+//fruits.shift()
+//console.log(fruits)
+//fruits.unshift('grapes') 
+//console.log(fruits)
+//let copiedfruits = fruits.slice(2 ,4)
+//console.log(copiedfruits)
+//fruits.splice(2, 0 , "pineapple")
+//console.log(fruits)
